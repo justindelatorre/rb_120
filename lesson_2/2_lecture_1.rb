967909299ab0be062eb1ea4db0c7e722e73378f3
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-06-20 19:08:47 +0000

    Add files from RB120 > Small Problems > Basics 3 and Lesson 2.2

---
+++ lesson_2/2_lecture_1.rb
@@ -0,0 +1,16 @@
+=begin
+Given the below usage of the Person class, code the class definition.
+=end
+
+class Person
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new('bob')
+p bob.name
+bob.name = 'Robert'
+p bob.name
