4f04eec8c04b656c587a8888fc535ba712f3b2cb
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-06-20 21:20:36 +0000

    Add files for RB120 > Small Problems > Basics 3 exercises

---
+++ small_problems/basics_3/8_method_lookup_2.rb
@@ -0,0 +1,26 @@
+=begin
+Using the following code, determine the lookup path used when invoking
+cat1.color. Only list the classes and modules that Ruby will check when
+searching for the #color method.
+=end
+
+class Animal
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new
+cat1.color
+
+=begin
+Method lookup path for #color:
+Cat
+Animal
+Object
+Kernel
+BasicObject
+=end
