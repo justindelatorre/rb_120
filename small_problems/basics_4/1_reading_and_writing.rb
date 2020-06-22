0c075d65dfadae4b6e3e069907e84f2276558cf6
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-06-22 19:18:49 +0000

    Add files for RB120 > Small Problems > Basics 4 exercises

---
+++ small_problems/basics_4/1_reading_and_writing.rb
@@ -0,0 +1,11 @@
+=begin
+Add the appropriate accessor methods to the following code.
+=end
+
+class Person
+  attr_accessor :name
+end
+
+person1 = Person.new
+person1.name = 'Jessica'
+puts person1.name
