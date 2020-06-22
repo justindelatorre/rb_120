fe748531871c64f3fc96b8ae2ac5319881302014
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-06-22 20:34:24 +0000

    Add files for RB120 > Small Problems > Basics 4 exercises

---
+++ small_problems/basics_4/10_unexpected_change.rb
@@ -0,0 +1,21 @@
+=begin
+Modify the following code to accept a string containing a first and last name.
+The name should be split into two instance variables in the setter method, then
+joined in the getter method to form a full name.
+=end
+
+class Person
+  def name=(n)
+    names = n.split
+    @first_name = names.first
+    @last_name = names[-1]
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+end
+
+person1 = Person.new
+person1.name = 'John Doe'
+puts person1.name
