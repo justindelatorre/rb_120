187725b772aca7b246d2c20846a2cf56320dde8a
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-06-26 20:23:31 +0000

    Add skeleton file for OO Tic Tac Toe

---
+++ lesson_5/modules_2_to_7/oo_ttt.rb
@@ -0,0 +1,44 @@
+=begin
+Assignment: https://launchschool.com/lessons/97babc46/assignments/7dcb53f1
+=end
+
+class Board
+  def initialize
+
+  end
+end
+
+class Square
+  def initialize
+
+  end
+end
+
+class Player
+  def initialize
+
+  end
+
+  def mark
+
+  end
+end
+
+class TTTGame
+  def play
+    display_welcome_message
+    loop do
+      display_board
+      first_player_moves
+      break if someone_won? || board_full?
+
+      second_player_moves
+      break if someone_won? || board_full?
+    end
+    display_result
+    display_goodbye_message
+  end
+end
+
+game = TTTGame.new
+game.play
