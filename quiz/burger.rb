3962c6b34ffcbf1f064e0bc945d8e7e72d26a04e
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-07-02 18:55:46 +0000

    Add file from RB120 Lesson 5 Quiz

---
+++ quiz/burger.rb
@@ -0,0 +1,87 @@
+=begin
+Quiz: https://launchschool.com/quizzes/5947ffb9/edit
+=end
+
+class Customer
+  attr_writer :order
+
+  def initialize
+    @order = nil
+  end
+
+  def place_order
+    @order = Order.new
+  end
+end
+
+class Order
+  def initialize
+    @burger = Burger.new
+    @side = Side.new
+    @drink = Drink.new
+  end
+
+  def meal
+    [@burger, @side, @drink]
+  end
+
+  def to_s
+    meal.map(&:to_s).join(', ')
+  end
+
+  def total
+    total_cost = @burger + @side + @drink
+    format("$%.2f", total_cost)
+  end
+end
+
+class MealItem
+  def initialize
+    @option = choose_option
+  end
+
+  def choose_option
+    puts "Please choose a #{self.class} option:"
+    puts item_options
+    gets.chomp
+  end
+
+  def item_options
+    self.class::OPTIONS.each do |num, option|
+      puts option[:name]
+    end
+  end
+
+  def to_s
+    self.class::OPTIONS[@option][:name]
+  end
+end
+
+class Burger < MealItem
+  OPTIONS = {
+    '1' => { name: 'LS Burger', cost: 3.00 },
+    '2' => { name: 'LS Cheeseburger', cost: 3.50 },
+    '3' => { name: 'LS Chicken Burger', cost: 4.50 },
+    '4' => { name: 'LS Double Deluxe Burger', cost: 6.00 }
+  }
+end
+
+class Side < MealItem
+  OPTIONS = {
+    '1' => { name: 'Fries', cost: 0.99 },
+    '2' => { name: 'Onion Rings', cost: 1.50 }
+  }
+end
+
+class Drink < MealItem
+  OPTIONS = {
+    '1' => { name: 'Cola', cost: 1.50 },
+    '2' => { name: 'Lemonade', cost: 1.50 },
+    '3' => { name: 'Vanilla Shake', cost: 2.00 },
+    '4' => { name: 'Chocolate Shake', cost: 2.00 },
+    '5' => { name: 'Strawberry Shake', cost: 2.00 }
+  }
+end
+
+my_order = Order.new
+my_order.choose_option
---
+++ small_problems/medium_1/2_fixed_array.rb
@@ -0,0 +1,79 @@
+=begin
+A fixed-length array is an array that always has a fixed number of elements.
+Write a class that implements a fixed-length array, and provides the necessary
+methods to support the below code.
+
+The code should output true 16 times.
+=end
+
+class FixedArray
+  attr_accessor :arr
+
+  def initialize(size)
+    @arr = Array.new(size, nil)
+  end
+
+  def [](idx)
+    @arr.fetch(idx)
+  end
+
+  def []=(idx, value)
+    self[idx]
+    @arr[idx] = value
+  end
+
+  def to_a
+    @arr.clone
+  end
+
+  def to_s
+    to_a.to_s
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+=begin
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+=end
