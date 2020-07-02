82c27f7efd9d83c12bf69b7ea42edef4faa93d06
Jay de la Torre <justin.d.delatorre@gmail.com>
2020-07-02 17:41:51 +0000

    Add file for RB120 > Small Problems > Medium 1.1 exercise

---
+++ small_problems/medium_1/1_privacy.rb
@@ -0,0 +1,24 @@
+=begin
+Consider the class below.
+
+Modify this class so both flip_switch and the setter method switch= are private
+methods.
+=end
+
+class Machine
+  def start
+    flip_switch(:on)
+  end
+
+  def stop
+    flip_switch(:off)
+  end
+
+  private
+
+  attr_writer :switch
+
+  def flip_switch(desired_state)
+    self.switch = desired_state
+  end
+end
